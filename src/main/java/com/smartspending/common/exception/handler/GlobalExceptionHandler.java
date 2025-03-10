478539a96689de2a13e4cc24006749bf5b435e6b
noah <qkrwldn0818@naver.com>
2025-03-10 21:43:11 +0900

    fix: GlobalExceptionHandler 수정

--- src/main/java/com/smartspending/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/smartspending/common/exception/handler/GlobalExceptionHandler.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -36,9 +38,21 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @Override
-    protected ResponseEntity<Object> handleExceptionInternal(Exception exception, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
-        log.error("Exception", exception);
-        CommonResponse<Object> response = ApiResponseUtil.failure(statusCode.value(), exception.getMessage());
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        log.error("Exception", ex);
+        CommonResponse<Object> response = ApiResponseUtil.failure(statusCode.value(), ex.getMessage());
         return ResponseEntity.status(statusCode).body(response);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.warn("ValidationException", ex);
+
+        FieldError fistFieldError = ex.getBindingResult().getFieldErrors().get(0);
+        CommonResponse<Object> response = ApiResponseUtil.failure(status.value(), fistFieldError.getField() + " : " + fistFieldError.getDefaultMessage());
+
+        return ResponseEntity.status(status).body(response);
+    }
+
+
 }
