e11463c4bfd16a27657bc52c60de6508ee87606e
noah <qkrwldn0818@naver.com>
2025-03-11 20:29:36 +0900

    feat: SecurityConfig 추가

---
+++ src/main/java/com/smartspending/common/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.smartspending.common.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*"); // 모든 Origin 허용
+        configuration.addAllowedMethod("*");       // 모든 HTTP Method 허용
+        configuration.addAllowedHeader("*");       // 모든 헤더 허용
+        configuration.setAllowCredentials(true);   // 인증 정보 포함 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable) // csrf 비활
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // cors 커스텀
+                .httpBasic(AbstractHttpConfigurer::disable) // httpBasic 비활
+                .formLogin(AbstractHttpConfigurer::disable) // formLogin 비활
+                .logout(AbstractHttpConfigurer::disable) // logout 설정 비활
+                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // session 비활
+                .headers((headersConfig) ->
+                        headersConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // X-Frame-Options 헤더를 비활성화
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .requestMatchers("/", "/user/login/**", "/user/logout/**", "/user/register/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
