36c71e7b09b4e4eb9ccfc9a663da548691bf2d6b
noah <qkrwldn0818@naver.com>
2025-03-13 21:06:59 +0900

    feat: Password Encoder

--- src/main/java/com/smartspending/common/exception/CommonResponseCode.java
+++ src/main/java/com/smartspending/common/exception/CommonResponseCode.java
@@ -10,7 +10,8 @@ public enum CommonResponseCode {
 
     SUCCESS(HttpStatus.OK, "성공"),
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러"),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "아이디 또는 비밀번호가 일치하지 않습니다");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "아이디 또는 비밀번호가 일치하지 않습니다"),
+    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 이메일 입니다");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/smartspending/user/service/UserServiceImpl.java
+++ src/main/java/com/smartspending/user/service/UserServiceImpl.java
@@ -6,7 +6,10 @@ import com.smartspending.user.dto.request.LoginRequestDto;
 import com.smartspending.user.dto.request.RegisterRequestDto;
 import com.smartspending.user.entity.User;
 import com.smartspending.user.repository.UserRepository;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,13 +18,16 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
     public Long registerUser(RegisterRequestDto requestDto) {
+        validateEmail(requestDto.getEmail());
+        String encodePassword = passwordEncoder.encode(requestDto.getPassword());
         User user = User.builder()
                 .email(requestDto.getEmail())
-                .password(requestDto.getPassword())
+                .password(encodePassword)
                 .name(requestDto.getName())
                 .emailVerified(requestDto.isEmailVerified())
                 .build();
@@ -31,13 +37,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Long login(LoginRequestDto requestDto) {
-        User user = userRepository.findByEmail(requestDto.getEmail());
-        if (user == null) {
-            throw new CustomException(CommonResponseCode.USER_NOT_FOUND);
-        }
-        if (!user.getPassword().equals(requestDto.getPassword())) {
+        User user = userRepository.findByEmail(requestDto.getEmail())
+                .orElseThrow(() -> new CustomException(CommonResponseCode.USER_NOT_FOUND));
+
+        if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
             throw new CustomException(CommonResponseCode.USER_NOT_FOUND);
         }
         return user.getId();
+    }
+
+    private void validateEmail(@NotBlank(message = "이메일을 입력해주세요") @Email(message = "올바른 형식이 아닙니다") String email) {
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new CustomException(CommonResponseCode.EMAIL_ALREADY_EXISTS);
+        }
     }
 }
