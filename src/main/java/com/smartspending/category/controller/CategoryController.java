183504a44a7eaf08aa08862f84c6652033cd01e6
noah <qkrwldn0818@naver.com>
2025-04-07 17:45:22 +0900

    feat: category controller 생성

---
+++ src/main/java/com/smartspending/category/controller/CategoryController.java
@@ -0,0 +1,39 @@
+package com.smartspending.category.controller;
+
+import com.smartspending.category.dto.request.CategoryRequestDto;
+import com.smartspending.category.dto.request.CategoryUpdateDto;
+import com.smartspending.category.service.CategoryService;
+import com.smartspending.common.auth.UserDetailsImpl;
+import com.smartspending.common.response.CommonResponse;
+import com.smartspending.common.util.ApiResponseUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @PostMapping("/create")
+    public CommonResponse<Void> createCategory(@RequestBody CategoryRequestDto categoryRequestDto,
+                                               @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        categoryService.save(categoryRequestDto, userDetails.getUserId());
+        return ApiResponseUtil.success();
+    }
+
+    @PostMapping("/update")
+    public CommonResponse<Void> updateCategory(@RequestBody CategoryUpdateDto categoryUpdateDto,
+                                               @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        categoryService.update(categoryUpdateDto, userDetails.getUserId());
+        return ApiResponseUtil.success();
+    }
+
+    @DeleteMapping("/delete")
+    public CommonResponse<Void> deleteCategory(@RequestParam Long categoryId,
+                                               @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        categoryService.delete(categoryId, userDetails.getUserId());
+        return ApiResponseUtil.success();
+    }
+}
