7563969966b772c37f9df2ceb3ab3c7bc535f775
noah <qkrwldn0818@naver.com>
2025-04-29 21:28:42 +0900

    feat: dto 조건 추가

--- src/main/java/com/smartspending/budget/dto/request/BudgetRequestDto.java
+++ src/main/java/com/smartspending/budget/dto/request/BudgetRequestDto.java
@@ -2,6 +2,8 @@ package com.smartspending.budget.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.smartspending.budget.enums.BudgetPeriodType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +15,21 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class BudgetRequestDto {
 
+    @NotNull(message = "예산을 입력해주세요")
+    @Positive(message = "양수만 입력 가능합니다")
     private BigDecimal amount;
+
+    @NotNull
     private BudgetPeriodType periodType;
+
+    @NotNull(message = "기간을 입력해주세요")
     private Integer period;
+
+    @NotNull
     @JsonProperty("isActive")
     private boolean isActive;
+
+    @NotNull
     @JsonProperty("isRecurring")
     private boolean isRecurring;
 
